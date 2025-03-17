13c170c2916a581aadeddf6898e4500332b9d7e8
Shakib Anwar Nibir <nibirshakib@gmail.com>
2025-03-17 23:42:22 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-bg": "../assets/bg-img.png",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
